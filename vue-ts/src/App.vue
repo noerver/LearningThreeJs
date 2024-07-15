7c5eba1d600e6897dbc7834f137fa20b66beb637
tom <337961290@qq.com>
2024-07-16 00:00:12 +0800

    feat: first vue ts component

--- vue-ts/src/App.vue
+++ vue-ts/src/App.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Demo1 from './components/Demo1.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Wellcome to learning three.js</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Demo1 msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ vue-ts/src/components/Demo1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Hello Demo 01</div>
+    <div id="content">{{ message }}</div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const message = ref("test")
+
+
+</script>
